5abaca7807ae042abfcd9464856fb3969639bef7
Aslam <128795641+AslamBadure@users.noreply.github.com>
2024-05-30 21:43:53 +0530

    Add files via upload

---
+++ programmingChallange/src/Basic_programs/C1.java
@@ -0,0 +1,13 @@
+
+public class C1 {
+public static void main(String[] args) {
+	
+
+	char[] ch= {'k','a','n','c','h','a','n','a'};
+	 
+	for(int i=0;i<ch.length;i++) {
+		if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u') {
+		System.out.println(ch[i]);
+	}
+}
+}}
