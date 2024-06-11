54ba24d5096aff7c447bc44797f428f6466cc830
Aslam <128795641+AslamBadure@users.noreply.github.com>
2024-06-11 20:05:01 +0530

    Add files via upload

---
+++ programmingChallange/src/Note/Find_vowels.java
@@ -0,0 +1,21 @@
+package Note;
+
+public class Find_vowels {
+
+	public static void main(String[] args) {
+
+		char ch='b';
+		switch(ch)
+		{
+		case 'a':
+		case 'b':
+		case 'c':
+			System.out.println(ch+ " :is a vowels");
+			break;
+
+		default:
+			System.out.println(ch +" :is a consuntes");
+		}
+	}
+
+}
