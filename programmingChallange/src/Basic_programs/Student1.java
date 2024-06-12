ce844cc7fdf550ac5e417aebea028271c91bc32d
Aslam <128795641+AslamBadure@users.noreply.github.com>
2024-06-12 23:55:23 +0530

    Add files via upload

---
+++ programmingChallange/src/Basic_programs/Student1.java
@@ -0,0 +1,16 @@
+package oops;
+
+class A
+{
+	int id=10;
+	String name="Aslam";
+}
+public class Student1
+{
+	public static void main(String[] args) 
+	{
+		A a= new A();
+		System.out.println(a.id);
+		System.out.println(a.name);
+	}
+}
---
+++ programmingChallange/src/Basic_programs/Student2.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class Student2 
+{
+	int id;
+	String name;
+	public static void main(String[] args) 
+	{
+		Student2 s2 = new Student2();
+		s2.id=10;
+		s2.name= "Aslam";
+		System.out.println(s2.id+" "+s2.name);
+	}
+
+}
