f668fdb6f9683e54fb720b448d7e1abf21297e02
Aslam <128795641+aslambadure@users.noreply.github.com>
2024-08-04 22:26:16 +0530

    Add files via upload

---
+++ programmingChallange/src/Numbers/Fibonacci.java
@@ -0,0 +1,20 @@
+package Basic_program;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+
+		
+		int n1=0;
+		int n2=1;
+		int n3;
+		int count=10;
+		System.out.println(n1+""+n2);
+		for(int i=0;i<=count;i++) {
+			n3=n1+n2;
+			System.out.println(n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+	}
+}
