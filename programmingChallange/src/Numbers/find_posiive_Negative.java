fcb811712351ca2f78e35cebe0d67051bc2f6fd1
Aslam <128795641+aslambadure@users.noreply.github.com>
2024-08-09 21:53:02 +0530

    Add files via upload

---
+++ programmingChallange/src/Numbers/find_posiive_Negative.java
@@ -0,0 +1,16 @@
+package Basic_program;
+
+public class find_posiive_Negative {
+	public static void main(String[] args) {
+		int num=12;		
+		if(num>0) {
+			System.out.println("postive number");
+		}else if(num<0){
+			System.out.println("negative number");
+		}
+		else
+		{
+			System.out.println("others");
+		}
+	}
+}
