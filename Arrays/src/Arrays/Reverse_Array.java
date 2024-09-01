38e3d8f2f0633ea4b911494d8c4e7ca966b714f0
Aslam <128795641+aslambadure@users.noreply.github.com>
2024-09-01 21:46:53 +0530

    Add files via upload

---
+++ Arrays/src/Arrays/Reverse_Array.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Reverse_Array {
+	public static void main(String[] args) {
+
+
+		int[] arr= {1,2,3,4,5,6};
+
+		int n=arr.length;
+		for(int i=n-1;i>=0;i--) 
+			System.out.println("Reverse of array: "+arr[i]);	
+		
+	}
+}
